1b17caf66d517ca891b1c24c6fe563325933bed0
Luis B <luis.beroiza@gmail.com>
2023-05-22 09:51:55 +0200

    Recibe Datos del formulario
    se usa RequestParams

--- src/main/java/com/springboot/form/app/controllers/FormController.java
+++ src/main/java/com/springboot/form/app/controllers/FormController.java
@@ -4,22 +4,27 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class FormController {
 
-  @GetMapping("/form") 
-  public String showForm(Model model){
+  @GetMapping("/form")
+  public String showForm(Model model) {
+    model.addAttribute("title", "Create new User");
     return "form/show_form";
-    
+
   }
 
-  
-  @PostMapping("/form") 
-  public String processForm(Model model){
+  @PostMapping("/form")
+  public String processForm(Model model, @RequestParam String username, @RequestParam String password,
+      @RequestParam String email) {
+    model.addAttribute("title", "User creation successful");
+    model.addAttribute("username", username);
+    model.addAttribute("password", password);
+    model.addAttribute("email", email);
     return "form/show_result";
-    
-  }
 
+  }
 
 }
