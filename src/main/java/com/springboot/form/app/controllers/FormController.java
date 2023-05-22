d0e70926311c04b58734c26c0e02c23ab2fd1c51
Luis B <luis.beroiza@gmail.com>
2023-05-22 10:39:18 +0200

    Agrega Validaciones.
    Con la anotacion @Valid (Spring Boot 2 desde javax y en Spring Boot 3 desde jakarta)
    se pueden agregar Validaciones a los campos.
    Las validaciones fueron definidas en la clase User.
    
    El Objeto BindingResult contiene las validaciones del objeto anterior.
    debe ser inyectado como segundo argumento del metodo, al lado del objeto con la anotacion
    @Valid.

--- src/main/java/com/springboot/form/app/controllers/FormController.java
+++ src/main/java/com/springboot/form/app/controllers/FormController.java
@@ -1,11 +1,17 @@
 package com.springboot.form.app.controllers;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import com.springboot.form.app.models.User;
+
+import jakarta.validation.Valid;
 
 @Controller
 public class FormController {
@@ -17,8 +23,26 @@ public class FormController {
 
   }
 
+//  Con la anotacion @Valid (Spring Boot 2 desde javax y en Spring Boot 3 desde jakarta)
+//  se pueden agregar Validaciones a los campos.
+//  Las validaciones fueron definidas en la clase User.
+//
+//  El Objeto BindingResult contiene las validaciones del objeto anterior.
+//  debe ser inyectado como segundo argumento del metodo, al lado del objeto con la anotacion
+//  @Valid.
   @PostMapping("/form")
-  public String processForm(User user, Model model) {
+  public String processForm(@Valid User user,BindingResult result, Model model) {
+
+    if (result.hasErrors()) {
+      model.addAttribute("title", "User creation failed");
+      Map<String, String> errors = new HashMap<>();
+      result.getFieldErrors().forEach(field -> {
+        errors.put(field.getField(), String.format("The Field '%s' has errors: %s", field.getField(), field.getDefaultMessage()));
+      });
+      model.addAttribute("error", errors);
+      return "form/show_form";
+    }
+
     model.addAttribute("title", "User creation successful");
     model.addAttribute("user", user);
     return "form/show_result";
--- src/main/java/com/springboot/form/app/models/User.java
+++ src/main/java/com/springboot/form/app/models/User.java
@@ -1,13 +1,22 @@
 package com.springboot.form.app.models;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
+
 // Al Poner la clase User como primer argumento del metodo "processForm"
 // puede mapear los datos del formulario a los campos de la clase
 // porque la clase tiene los mismo nombres de variables que los name
 // del formulario.
 public class User {
 
+  @NotEmpty
   private String username;
+
+  @NotEmpty
   private String password;
+
+  @NotEmpty
+  @Email
   private String email;
 
   public String getUsername() {
